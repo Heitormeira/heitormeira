5944c26681edc30e61b5b93d8481365bc15253c2
Heitormeira <heitormeira14@gmail.com>
2025-02-13 12:02:41 +0000

    variaveis em c

---
+++ qwewqe/nautico.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n1 = 5;
+    double n2 = 3.1801;
+    char letra =  'a';
+
+    printf("Digite um valor inteiro: ");
+    scanf("%d",&n1);
+    printf("n1 = %d\n", n1);
+    printf("n2 - %f\n", n2);
+    printf("letra = %c\n", letra);
+    printf("Ola turma!\n");
+    return 0;
+}
